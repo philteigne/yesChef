422698eb49eff89b9c9f8cfa3e611f4f1f6d7722
Cody <codylau6233@hotmail.com>
2024-04-17 23:55:33 -0700

    Add routes to ingredientsApiRoutes and savedRecipesApi

--- backend/routes/ingredientsApiRoutes.js
+++ backend/routes/ingredientsApiRoutes.js
@@ -2,19 +2,42 @@ const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
 
-router.get('/', (req, res) => {
-  // const query = `SELECT * FROM widgets`;
-  // console.log(query);
-  // db.query(query)
-  //   .then(data => {
-  //     const widgets = data.rows;
-  //     res.json({ widgets });
-  //   })
-  //   .catch(err => {
-  //     res
-  //       .status(500)
-  //       .json({ error: err.message });
-  //   });
+router.get('/:userId', (req, res) => {
+  // Retrieve list of users ingredients list
+
+  const userId = req.params.userId;
+
+  const mockIngredients = [
+    "pasta",
+    "chicken",
+    "pesto",
+    "tomato paste"
+  ];
+
+  res.json(mockIngredients);
+
 });
+
+router.post('/:userId', (req, res) => {
+  // Add a new ingredient to the users saved list
+  const userId = req.params.userId;
+  
+})
+
+router.put('/:userId/:ingredientId', (req, res) => {
+  // Change the name of a users stored ingredient
+
+  const userId = req.params.userId;
+  const ingredientId = req.params.ingredientId;
+
+
+})
+
+router.delete('/:userId/:ingredientId', (req, res) => {
+  const userId = req.params.userId;
+  const ingredientId = req.params.ingredientId;
+
+  // Delete saved ingredient from users ingredients list
+})
 
 module.exports = router;
