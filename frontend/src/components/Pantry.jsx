5482510f9b35b507f5c7771a5e35bb991e9c3ee1
Phil <philippe.teigne@gmail.com>
2024-04-22 19:10:38 -0400

    Create ListItem container Pantry component and update links

---
+++ frontend/src/components/Pantry.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import IngredientList from './IngredientList';
+
+const Pantry = () => {
+  return (
+    <div>
+      <div>
+        <h2>Pantry</h2>
+        <div>
+          
+        </div>
+      </div>
+      <IngredientList />
+    </div>
+  )
+}
+
+export default Pantry;
