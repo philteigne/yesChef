9836d58d97674daac78a535bf0e8cdb821534b31
Phil <philippe.teigne@gmail.com>
2024-04-23 17:18:23 -0400

    merge components files into one folder

---
+++ frontend/src/Components/IngredientList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import IngredientListItem from "./IngredientListItem";
+
+import { List, Box } from '@mui/material'
+
+const IngredientList = ({ingredientListTest, deleteIngredient}) => {
+  return(
+    <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+      <List>
+        {ingredientListTest.map((ingredient) => {
+          return(
+            <IngredientListItem
+              ingredient={ingredient}
+              key={ingredient.id}
+              deleteIngredient={deleteIngredient}
+            />
+          )
+        })}
+      </List>
+    </Box>
+  )
+}
+
+export default IngredientList;
---
+++ frontend/src/Components/IngredientListItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { ListItem, ListItemText, IconButton, Box } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const IngredientListItem = ({ingredient, deleteIngredient}) => {
+
+
+  return (
+    <Box sx={{ flexGrow: 1, maxWidth: 300 }}>
+      <ListItem
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete" onClick={() => deleteIngredient(ingredient)}>
+            <DeleteIcon />
+          </IconButton>
+        }
+      >
+        <ListItemText
+          primary={ingredient.name}
+        />
+      </ListItem>
+    </Box>
+  )
+}
+
+export default IngredientListItem;
