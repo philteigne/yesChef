b57a198915d2615d45cd1053593eeed35b8fd78a
Phil <philippe.teigne@gmail.com>
2024-04-24 00:16:36 -0400

    build out get ingredients/:userId route

--- backend/routes/ingredientsApi.js
+++ backend/routes/ingredientsApi.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
-const { getRecipeIngredients } = require('../db/queries/users');
+const { getIngredients, getRecipeIngredients } = require('../db/queries/users');
 
 router.get('/:userId', (req, res) => {
   // Retrieve list of users ingredients list
+  const userId = req.params.userId;
 
-;
+  getIngredients(userId)
+    .then(recipes => {
+      res.json(recipes);
+    })
+    .catch(error => {
+      console.error('Error fetching saved ingredients:', error);
+      res.status(500).json({ error: "Internal server error" });
+    });
 
 });
 
 router.get('/recipe/:recipeId', (req, res) => {
   // retrieve all ingredients for a given recipe
-  const recipeId = req.params.recipeId
+  const recipeId = req.params.recipeId;
 
   getRecipeIngredients(recipeId)
-  .then(ingredients => {
-    res.json(ingredients);
-  })
-  .catch(error => {
-    console.error('Error fetching saved ingredients:', error);
-    res.status(500).json({ error: "Internal server error" });
-  });
+    .then(ingredients => {
+      res.json(ingredients);
+    })
+    .catch(error => {
+      console.error('Error fetching saved ingredients:', error);
+      res.status(500).json({ error: "Internal server error" });
+    });
 });
 
 router.post('/:userId', (req, res) => {
