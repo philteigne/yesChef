d1a413512d317577c90874fd086849f8e8fa3894
Phil <philippe.teigne@gmail.com>
2024-04-24 00:19:01 -0400

    implement useReducer, store fetched ingredients in state

--- frontend/src/hooks/customHook.js
+++ frontend/src/hooks/customHook.js
@@ -1,39 +1,64 @@
-import { useState } from 'react';
+import { useReducer, useEffect } from 'react';
 
+export const API_CALL_URL = "http://localhost:8080/api/"
+const userId = 1
+
+export const INITIAL_STATE = {
+  ingredientList: [],
+}
+
+export const ACTIONS = {
+  GET_INGREDIENTS_USER: "GET_INGREDIENTS_USER" 
+}
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.GET_INGREDIENTS_USER:
+      return {
+        ...state,
+        ingredientList: action.payload
+      }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 const useApplicationData = () => {
 
-  const initialState = {
-    ingredientList: [
-      { id: 1, user_id: 1, name: 'Flour', quantity: 500.00, units: 'grams' },
-      { id: 2, user_id: 1, name: 'Sugar', quantity: 200.00, units: 'grams' },
-      { id: 3, user_id: 1, name: 'Salt', quantity: 5.00, units: 'grams' },
-      { id: 4, user_id: 1, name: 'Yeast', quantity: 15.00, units: 'grams' },
-      { id: 5, user_id: 1, name: 'Milk', quantity: 250.00, units: 'ml' },
-      { id: 6, user_id: 1, name: 'Butter', quantity: 100.00, units: 'grams' },
-      { id: 7, user_id: 1, name: 'Eggs', quantity: 3.00, units: 'units' },
-      { id: 8, user_id: 1, name: 'Baking Powder', quantity: 10.00, units: 'grams' },
-      { id: 9, user_id: 1, name: 'Cocoa Powder', quantity: 50.00, units: 'grams' },
-      { id: 10, user_id: 1, name: 'Vanilla Extract', quantity: 5.00, units: 'ml' }
-    ],
-  }
-  
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
-  const [ingredientListTest, setIngredientListTest] = useState(initialState.ingredientList);
+  // fetch ingredients from backend
+  useEffect(() => {
+    fetch(`${API_CALL_URL}ingredients/${userId}`)
+    .then((res) => res.json())
+    .then((data) => dispatch({ type: ACTIONS.GET_INGREDIENTS_USER, payload: data }))
+  }, []);
+
+  // delete an ingredient from backend 
+  useEffect(() => {
+    fetch(`${API_CALL_URL}ingredients/${userId}/`, {
+      method: 'DELETE'
+    })
+  }, [])
   
   const deleteIngredient = (ingredient) => {
-    setIngredientListTest(
-      ingredientListTest.filter((item) => item.id !== ingredient.id)
-    )
+    // setIngredientListTest(
+    //   ingredientListTest.filter((item) => item.id !== ingredient.id)
+    // )
+    return true
   }
   
   const addIngredient = (ingredient) => {
-    setIngredientListTest([...ingredientListTest, ingredient])
+    // setIngredientListTest([...ingredientListTest, ingredient])
+    return true
   }
+
 
   // calling useApplicationData function return these functions that changes states
   return {
-    ingredientListTest,
+    state,
     deleteIngredient,
     addIngredient
   };
