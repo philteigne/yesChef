01621980134d946c7a9f35b2871346b80b133111
Phil <philippe.teigne@gmail.com>
2024-04-29 00:38:56 -0400

    create temporary theme object and pass it to components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import Pantry from './Components/Pantry';
 
@@ -13,7 +12,101 @@ import ButtonAppBar from './Components/Navigation';
 import { applicationContext } from './hooks/applicationContext';
 import useApplicationData from './hooks/customHook';
 import Parameters from './Components/Parameters';
-import { Stack } from '@mui/material'; 
+import { Stack, Box } from '@mui/material';
+
+
+import { createTheme, ThemeProvider} from '@mui/material/styles'
+import './assets/fonts.css';
+
+const theme = createTheme({
+  palette: {
+    common: {
+      black: '#4A4A45',
+      white: '#EAE7DC',
+    },
+    primary: {
+      main: '#E85A4F',
+      contrastText: "#EAE7DC"
+    },
+    secondary: {
+      main: '#4A4A45'
+    },
+    text: {
+      primary: '#4A4A45'
+    },
+    background: {
+      paper: '#EAE7DC',
+      default: '#EAE7DC',
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: "El Messiri",
+      fontSize: 27,
+      fontWeight: 700,
+    },
+    h2: {
+      fontFamily: "El Messiri",
+      fontSize: 21,
+      fontWeight: 600,
+    },
+    h3: {
+      fontFamily: 'Kanit',
+      fontSize: 15,
+      fontWeight: 600,
+    }
+  },
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        color: "transparent",
+        elevation: 0,
+        position: "static"
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: "#4A4A45"
+        }
+      }
+    },
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          fill: "#4A4A45"
+        }
+      }
+    },
+    MuiTextField: {
+      defaultProps: {
+        color: 'secondary'
+      },
+      styleOverrides: {
+        root: {
+          '& .MuiInputBase-input': {
+            color: 'darkblue',
+          },
+          '& .MuiInputLabel-root': {
+            color: 'green',
+          },
+          '& .MuiOutlinedInput-root': {
+            borderWidth: 5,
+          },
+        }
+      }
+    }
+    // MuiInputBase: {
+    //   styleOverrides: {
+    //     root: {
+    //       border: `2px solid #4A4A45`,
+    //       fontFamily: 'El Messiri',
+
+    //     },
+    //   }
+    // }
+  }
+})
 
 function App() {
   
@@ -22,22 +115,23 @@ function App() {
   return (
     // wrapping context
     // value contains all the functions from useApplicationData that alter states
-    
-    <applicationContext.Provider value={{state, dispatch}}>
-      <div>
-        <ButtonAppBar />
-        <Stack
-          direction="row"
-        >
-          <Pantry  />
-          <Parameters />
-        </Stack>
-        <div className="App">
-          <RecipeListView />
-          <RecipeFullView />
-        </div>
-      </div>
-      </applicationContext.Provider>
+    <ThemeProvider theme={theme}>
+      <applicationContext.Provider value={{state, dispatch}}>
+        <Box sx={{ backgroundColor: '#EAE7DC',}}>
+          <ButtonAppBar />
+          <Stack
+            direction="row"
+          >
+            <Pantry  />
+            <Parameters />
+          </Stack>
+          <div className="App">
+            <RecipeListView />
+            <RecipeFullView />
+          </div>
+        </Box>
+        </applicationContext.Provider>
+    </ThemeProvider>
   );
 }
 
