9e458494a571dabb26e6571c853ed99a2992b834
Cody <codylau6233@hotmail.com>
2024-05-07 19:34:16 -0700

    Refactor App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import Pantry from './Components/Pantry';
+
 import { Redirect } from "react-router-dom";
 import CreateRecipe from './Components/CreateRecipe.jsx';
-import RecipeListView from './Components/RecipeListView';
-import RecipeFullView from './Components/RecipeFullView';
 import ButtonAppBar from './Components/Navigation';
 import { applicationContext } from './hooks/applicationContext';
 import useApplicationData from './hooks/customHook';
-import Parameters from './Components/Parameters';
-import { Stack } from '@mui/material'; 
 import HomePage from './Components/HomePage.jsx';
-import RecipeResponseView from './Components/RecipeResponseView.jsx';
-import Loading from './Components/Loading.jsx';
 import SignInSide from './Components/Login.jsx';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline'
 
-
 import { ThemeProvider} from '@mui/material/styles'
 import customColorScheme from './styles/MuiTheme';
 import './assets/fonts/fonts.css';
+import ViewRecipe from './Components/ViewRecipe.jsx';
 
 
 function App() {
@@ -45,19 +39,8 @@ function App() {
               <Switch>
                 <Route path='/' exact component={HomePage}/>
                 <Route path='/login' component={SignInSide} />
-
                 <PrivateRoute path="/create-recipe" component={CreateRecipe} auth={state.isLoggedIn} />
-
-                <Router path="/view-recipe">
-                  <Stack
-                    direction="row"
-                    justifyContent='center'
-                    spacing={12}
-                    >
-                    <RecipeListView />
-                    <RecipeFullView />
-                  </Stack>
-                </Router>
+                <PrivateRoute path="/view-recipe" component={ViewRecipe} auth={state.isLoggedIn} />
               </Switch>
             </div>
           </applicationContext.Provider>
