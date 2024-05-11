aed15dbbbefc810d638bd778baf1442536b58251
Phil <philippe.teigne@gmail.com>
2024-05-11 00:37:38 -0400

    add scroll to bottom on ingredient input

--- frontend/src/Components/IngredientList.jsx
+++ frontend/src/Components/IngredientList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 
 import IngredientListItem from './IngredientListItem';
 import { applicationContext } from '../hooks/applicationContext';
@@ -7,17 +7,31 @@ import { Box } from '@mui/material';
 
 const IngredientList = () => {
 
-  const { state } = useContext(applicationContext)
+  const { state } = useContext(applicationContext);
+
+  // AUTO SCROLL TO BOTTOM
+  const ingredientsEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    ingredientsEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [state.ingredientList]);
 
   return(
     <Box sx={{ display: 'flex', flexWrap:'wrap', height: '300px', overflow: "auto" }}>
         {state.ingredientList.map((ingredient) => {
           return(
-            <IngredientListItem
-              ingredient={ingredient}
-              key={ingredient.id}
-              sx={{ width: '100%' }}
-            />
+            <React.Fragment>
+              <IngredientListItem
+                ingredient={ingredient}
+                key={ingredient.id}
+                sx={{ width: '100%' }}
+              />
+              <div ref={ingredientsEndRef} />
+            </React.Fragment>
           )
         })}
     </Box>
