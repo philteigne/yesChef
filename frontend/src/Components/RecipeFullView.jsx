25791325664cfd3a2460ba4f0ef50b5423c41201
Phil <philippe.teigne@gmail.com>
2024-05-11 18:56:39 -0400

    match styling of recipeFullView to RecipeGeneration

--- frontend/src/Components/RecipeFullView.jsx
+++ frontend/src/Components/RecipeFullView.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 
 import { applicationContext } from "../hooks/applicationContext";
 
-import { Box, Typography, Paper, IconButton, ListItem } from "@mui/material";
+import { Box, Typography, Paper, IconButton, ListItem, Avatar } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import kirby from '../assets/images/kirby-chocolate-pie.png';
 
@@ -16,100 +16,88 @@ function RecipeFullView() {
   
   function splitRecipe(recipe) {
     const arr = recipe.split(/\d+\./).filter(step => step.trim() !== '');
-   return arr.map((step, index) => {
+    return arr.map((step, index) => {
       return `${index + 1}.${step}`
     })
   }
 
-  
-  if (!recipe) {
-    return (
-      <Box sx={{ width: 0.43, display: 'flex', flexDirection: 'column'}}>
-        <Typography
-        variant="h1"
-        component="h1"
-        color="primary"
-        >
-          &#8226; recipe viewer
-        </Typography>
-        <Typography variant="h2" sx={{marginTop: 3, marginBottom: 3}}>
-          Please select or create a recipe!
-        </Typography>
-        <Box>
-          <img src={kirby} alt="chef kirby holding a chocolate pie" style={{width: 'auto', height: '70%'}}></img>
-        </Box>
-      </Box>
-    )
-    
+  if (recipe && !Array.isArray(recipe.steps)) {
+    recipe.steps = splitRecipe(recipe.steps)
   }
 
   return (
-    <Box sx={{ width: 0.43}}>
-      
-      <Typography
-        variant="h1"
-        component="h1"
-        color="primary"
-      >
-        &#8226; recipe viewer
-      </Typography>
-      
-      <Box sx={{
-        flexGrow: 1,
-        width: 1,
-        maxWidth: 1,
-        height: 43,
-        maxHeight: 43,
-        border: '2px solid',
-        borderColor: state.themeColors.textColor,
-        borderRadius: '10px',
-        overflow: 'hidden',
-        m: 0.6
+    <Box sx={{ 
+      margin: 2,
+      borderRadius: '4px', 
+      width: '0.43'
       }}>
-        <ListItem
-          sx={{alignItems: "center"}}
-          secondaryAction={
-            <IconButton
-              edge="end"
-              aria-label="delete"
-              onClick={() => dispatch({type: "EDIT_CURRENT_RECIPE", payload: recipe.id})}
-            >
-              <EditIcon color="primary" />
-            </IconButton>
-          }
-        >
-          <Typography
-            variant="h2"
-            component="h2"
-          >
-            {recipe.title} - {recipe.tags}
-          </Typography>
-        </ListItem>
-      </Box>
-        
-      <Paper sx={{ margin: 'auto', mt: 2, mb: 2, padding: 2, height: 1, overflow: 'auto' }}>
+      
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', direction: 'row', marginRight: 4.2, marginBottom: 0.5 }}>
         <Typography
-          variant="h2" component="h2" color="secondary"
+          variant="h1"
+          component="h1"
+          color="primary"
         >
-          {Array.isArray(recipeIngredients) ? recipeIngredients.map(ing => `${ing.name} - ${ing.quantity} ${ing.units} \n`).join(', ') : "No ingredients"}
+          &#8226; recipe generation
         </Typography>
-      <Paper sx={{ margin: 'auto', mt: 2, mb: 2, padding: 2 }}>
-        {/* function to map steps by number and add line breaks */}
-      {splitRecipe(recipe.steps).map((step) => {
-        return (
-          <Typography 
-          variant="h2"
-          component="h2"
-          color="secondary" 
-          sx={{ marginTop: 1, marginBottom: 1 }}
-          >
-            {step}
-          </Typography>
-        )
-      })}
-      </Paper>
-      </Paper>
 
+        {/* Recipe Generation Control */}
+        <Box>
+          <IconButton>
+            <Avatar sx={{ bgcolor: state.themeColors.accentColor }}>
+              <EditIcon
+                fontSize="20"
+                sx={{ fill: state.themeColors.bgColor}}
+              />
+            </Avatar>
+          </IconButton>
+        </Box>
+      </Box>
+
+      {recipe &&
+        <Box sx={{overflow: 'auto', height: '474px', ml: 1}}>
+          {/*RECIPE TITLE AND TAGS*/}
+          <Typography variant="h2" component="h2">
+            {recipe.title}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {recipe.tags}
+          </Typography>
+  
+          <Paper sx={{ margin: 'auto', mb: 2, padding: 1, pr: 1 }}>
+            {/*RECIPE INGREDIENTS*/}
+            {recipeIngredients.map(ing => {
+              return(
+                <Typography variant="body2" component="p" key={ing.id}>
+                  &#8226; {ing.name} - {ing.quantity} {ing.units}
+                </Typography>
+              )
+            })}
+            {/*RECIPE STEPS*/}
+            {recipe.steps.map((step) => {
+              return (
+                <Typography 
+                  variant="body2"
+                  component="p" 
+                  sx={{ marginTop: 1, marginBottom: 1 }}
+                >
+                  {step}
+                </Typography>
+              )
+            })}
+          </Paper>
+        </Box>
+      }
+      {!recipe &&
+        <Box sx={{ width: 0.43, display: 'flex', flexDirection: 'column'}}>
+          <Typography variant="h2" sx={{marginTop: 3, marginBottom: 3}}>
+            Please select or create a recipe!
+          </Typography>
+          <Box width='0.5'>
+            <img src={kirby} alt="chef kirby holding a chocolate pie" style={{width: 'auto', height: '50%'}}></img>
+          </Box>
+        </Box>
+      }
     </Box>
   )
 }
