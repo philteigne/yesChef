12ec974532a47c3d8a9f58540ec5bece47f84e18
Phil <philippe.teigne@gmail.com>
2024-05-11 01:08:45 -0400

    remove scroll to bottom as it breaks chatInput scroll

--- frontend/src/Components/IngredientList.jsx
+++ frontend/src/Components/IngredientList.jsx
@@ -9,31 +9,17 @@ const IngredientList = () => {
 
   const { state } = useContext(applicationContext);
 
-  // AUTO SCROLL TO BOTTOM
-  const ingredientsEndRef = useRef(null)
-
-  const scrollToBottom = () => {
-    ingredientsEndRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  useEffect(() => {
-    scrollToBottom()
-  }, [state.ingredientList]);
-
   return(
     <Box sx={{ display: 'flex', flexWrap:'wrap', height: '300px', overflow: "auto" }}>
-        {state.ingredientList.map((ingredient) => {
-          return(
-            <React.Fragment>
-              <IngredientListItem
-                ingredient={ingredient}
-                key={ingredient.id}
-                sx={{ width: '100%' }}
-              />
-              <div ref={ingredientsEndRef} />
-            </React.Fragment>
-          )
-        })}
+      {state.ingredientList.map((ingredient) => {
+        return(
+            <IngredientListItem
+              ingredient={ingredient}
+              key={ingredient.id}
+              sx={{ width: '100%' }}
+            />
+        )
+      })}
     </Box>
   )
 }
