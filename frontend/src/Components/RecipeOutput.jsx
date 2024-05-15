629908e4ae694bfb5e9726f2b15f7d59f33ad666
Phil <philippe.teigne@gmail.com>
2024-05-15 19:49:09 -0400

    Create reusable RecipeOutput component

---
+++ frontend/src/Components/RecipeOutput.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Box, Typography, Paper } from '@mui/material';
+
+const RecipeOutput = ({recipe}) => {
+  return (
+    <Box sx={{overflow: 'auto', height: '474px', ml: 1}}>
+        {/*RECIPE TITLE AND TAGS*/}
+        {console.log("state.recipeResponse", recipe)}
+        <Typography variant="h2" component="h2">
+          {recipe.title}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {recipe.tags.join(', ')}
+        </Typography>
+
+        <Paper sx={{ margin: 'auto', mb: 2, padding: 1, pr: 1 }}>
+          {/*RECIPE INGREDIENTS*/}
+          {recipe.ingredients.map(ing => {
+            return(
+              <Typography variant="body2" component="p">
+                &#8226; {ing.name} - {ing.quantity} {ing.units}
+              </Typography>
+            )
+          })}
+          {/*RECIPE STEPS*/}
+          {recipe.steps.map((step) => {
+            return (
+              <Typography 
+                variant="body2"
+                component="p" 
+                sx={{ marginTop: 1, marginBottom: 1 }}
+              >
+                {step}
+              </Typography>
+            )
+          })}
+        </Paper>
+      </Box>
+  )
+}
+
+export default RecipeOutput;
