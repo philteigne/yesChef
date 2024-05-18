cc290b4a0ae8a7efc72f2c6c7a8c3a7f308b1665
Phil <philippe.teigne@gmail.com>
2024-05-18 15:35:26 -0400

    change default userid to null

--- frontend/src/hooks/customHook.js
+++ frontend/src/hooks/customHook.js
@@ -5,7 +5,7 @@ export const API_CALL_URL = "http://localhost:8080/api/"
 // Basic App State
 const INITIAL_STATE_APP = {
   // should we have a initial userId
-  userId: 3,
+  userId: null,
   // For handling save recipe button loading animation
   saveRecipeLoading: false,
   error: null,
